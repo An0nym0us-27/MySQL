d6f631d92945e60b0b82ea92f4b9f6eadd63c9f5
Andrew C. <98856300+An0nym0us-27@users.noreply.github.com>
2025-01-16 06:49:59 -0800

    Update DataRetrieval.sql

--- Library Management Project/DataRetrieval.sql
+++ Library Management Project/DataRetrieval.sql
@@ -1,1 +1,18 @@
+/* Retrieving all books with their authors*/
+SELECT BooksTable.Title, AuthorTable.Name 
+FROM BooksTable
+INNER JOIN AuthorTable ON BooksTable.AuthorID = AuthorTable.Author_ID;
 
+
+/* Retrieve how many books each author wrote*/
+SELECT AuthorTable.Name, COUNT(BooksTable.Book_ID) AS BookCount
+FROM AuthorTable
+LEFT JOIN BooksTable ON AuthorTable.Author_ID = BooksTable.AuthorID
+GROUP BY AuthorTable.Name;
+
+
+/* Return people and ID number for those that haven't returned their books*/
+SELECT BorrowersTable.Name, BorrowersTable.Borrower_ID
+FROM BorrowersTable
+INNER JOIN BorrowingRecordTable ON BorrowersTable.Borrower_ID = BorrowingRecordTable.BorrowerID
+WHERE BorrowingRecordTable.Return_Date IS NULL;
